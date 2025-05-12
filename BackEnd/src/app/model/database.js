ee96e7e3c001163628ba5436d4b9414bab2e2503
NgocAn3105 <ngocancd2017@gmail.com>
2025-05-13 02:19:01 +0700

    Update database.js

--- BackEnd/src/app/model/database.js
+++ BackEnd/src/app/model/database.js
@@ -2,7 +2,7 @@ const Mysql = require("mysql2");
 const pool = Mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "3105",
+    password: "",
     database: "petproject"
 })
 const db = pool.promise()
